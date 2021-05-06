01ff2d2621ea985cd78b577d765b33e0f097ece4
atulkumaryadav24 <82756198+atulkumaryadav24@users.noreply.github.com>
2021-05-06 18:21:37 +0530

    Create W1P1.java

---
+++ W1P1.java
@@ -0,0 +1,9 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class W1P1 {
+    public static void main(String[] args) {
+        for(int i='e'/'e';i<'e';i++){
+            System.out.println(i);
+        }
+    }
+}
